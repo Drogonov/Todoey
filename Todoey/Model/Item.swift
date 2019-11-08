945134972991be2c814a47a635441d7bdc1b5cf9
Anton Drogonov <vlezko.anton@yandex.ru>
2019-11-08 13:21:33 +0300

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Admin on 08/11/2019.
+//  Copyright © 2019 Anton Drogonov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
